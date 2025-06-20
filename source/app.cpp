bcb79c21183f6bcb8b47aa1df0a2908e0b69bb4e
silver_volt4 <gdpancake5@gmail.com>
2025-06-21 00:42:56 +0200

    graphic design is my passion

--- source/app.cpp
+++ source/app.cpp
@@ -52,6 +52,39 @@ void App::render()
     C3D_FrameEnd(0);
 }
 
+template <uint8_t S>
+void parseTemperature(Assets &assets, SpriteTextRenderer<S> &str, int8_t tempValue, bool celsius)
+{
+    if (tempValue < 0)
+    {
+        str.addChar(C2D_SpriteSheetGetImage(assets.numbers, 11)); // minus
+        tempValue = std::abs(tempValue);
+    }
+
+    bool leading = true;
+    for (uint8_t order = 100; order > 0; order /= 10)
+    {
+        uint8_t digit = tempValue / order;
+        if (digit || !leading)
+        {
+            leading = false;
+            str.addChar(C2D_SpriteSheetGetImage(assets.numbers, digit));
+        }
+        tempValue -= digit * order;
+    }
+
+    if (celsius)
+    {
+        str.addChar(C2D_SpriteSheetGetImage(assets.numbers, 10)); // °
+        str.addChar(C2D_SpriteSheetGetImage(assets.numbers, 12)); // C
+    }
+    else
+    {
+        str.addSpace();
+        str.addChar(C2D_SpriteSheetGetImage(assets.numbers, 13)); // index of F
+    }
+}
+
 class GlanceView
 {
     Assets *assets;
@@ -66,90 +99,16 @@ class GlanceView
         }
     } texts;
 
-    struct
-    {
-        float scale = 1.0;
-        C2D_Image image;
-    } images[8];
-    uint8_t textLength;
-
-    void renderGlanceView(float xPos, float yPos, float scale, C2D_ImageTint *tint)
-    {
-        constexpr float gap = 30;
-        constexpr float space = 60;
-        float width = 0;
-        float height = 0;
-
-        for (uint8_t i = 0; i < textLength; i++)
-        {
-            auto frame = images[i];
-            if (frame.scale == 0)
-            {
-                width += (space)*scale;
-            }
-            else
-            {
-                width += (frame.image.subtex->width + gap) * scale * frame.scale;
-                height = std::max(height, frame.image.subtex->height * scale * frame.scale);
-            }
-        }
-
-        xPos -= width / 2;
-        yPos -= height / 2;
-
-        for (uint8_t i = 0; i < textLength; i++)
-        {
-            auto frame = images[i];
-            if (frame.scale == 0)
-            {
-                xPos += (space)*scale;
-            }
-            else
-            {
-                C2D_DrawImageAt(frame.image, xPos, yPos + (height - frame.image.subtex->height * scale * frame.scale) / 2, 0, tint, scale * frame.scale, scale * frame.scale);
-                xPos += (frame.image.subtex->width + gap) * scale * frame.scale;
-            }
-        }
-    }
+    SpriteTextRenderer<8> str;
 
 public:
     GlanceView(Assets *assets, std::string name, int8_t tempValue, uint8_t weatherState, bool celsius) : assets(assets)
     {
         texts.setCityName(name);
 
-        textLength = 0;
+        str.addChar(C2D_SpriteSheetGetImage(assets->icons, weatherState), 2.0);
 
-        images[textLength++] = {2.0, C2D_SpriteSheetGetImage(assets->icons, weatherState)};
-        images[textLength++].scale = 0;
-
-        if (tempValue < 0)
-        {
-            images[textLength++].image = C2D_SpriteSheetGetImage(assets->numbers, 11); // minus
-            tempValue = std::abs(tempValue);
-        }
-
-        bool leading = true;
-        for (uint8_t order = 100; order > 0; order /= 10)
-        {
-            uint8_t digit = tempValue / order;
-            if (digit || !leading)
-            {
-                leading = false;
-                images[textLength++].image = C2D_SpriteSheetGetImage(assets->numbers, digit);
-            }
-            tempValue -= digit * order;
-        }
-
-        if (celsius)
-        {
-            images[textLength++].image = C2D_SpriteSheetGetImage(assets->numbers, 10); // °
-            images[textLength++].image = C2D_SpriteSheetGetImage(assets->numbers, 12); // C
-        }
-        else
-        {
-            images[textLength++].scale = 0;
-            images[textLength++].image = C2D_SpriteSheetGetImage(assets->numbers, 13); // index of F
-        }
+        parseTemperature(*assets, str, tempValue, celsius);
     }
 
     void draw(float scale)
@@ -160,10 +119,10 @@ public:
         constexpr float GLANCE_TEXT_Y = Screen::SCREEN_HEIGHT / 2;
 
         C2D_PlainImageTint(&tint, C2D_Color32(0, 0, 0, 100), 1);
-        renderGlanceView(GLANCE_TEXT_X + (10 * scale), GLANCE_TEXT_Y + (10 * scale), scale, &tint);
+        str.render(GLANCE_TEXT_X + (10 * scale), GLANCE_TEXT_Y + (10 * scale), scale, &tint);
 
         C2D_PlainImageTint(&tint, C2D_Color32(255, 255, 255, 255), 0);
-        renderGlanceView(GLANCE_TEXT_X, GLANCE_TEXT_Y, scale, &tint);
+        str.render(GLANCE_TEXT_X, GLANCE_TEXT_Y, scale, &tint);
 
         C2D_DrawImageAt(C2D_SpriteSheetGetImage(assets->gui, 0), 40, 0, 0);
         C2D_DrawText(&texts.cityName, C2D_AlignCenter | C2D_WithColor, 200, 6, 0, 0.8, 0.8, C2D_Color32(255, 255, 255, 255));
@@ -202,21 +161,43 @@ void App::renderTop()
     C2D_DrawText(&assets.staticText.R_next, C2D_AlignRight | C2D_AtBaseline | C2D_WithColor, Screen::TOP_SCREEN_WIDTH - PAGE_SWITCH_BUTTONS_OFFSET, Screen::SCREEN_HEIGHT - PAGE_SWITCH_BUTTONS_OFFSET, 0, 0.6, 0.6, whiteColor);
 }
 
+static SpriteTextRenderer<6> getBar(Assets &assets)
+{
+    SpriteTextRenderer<6> a;
+    parseTemperature(assets, a, 10, true);
+    return a;
+}
+
+static SpriteTextRenderer<1> getIcon(Assets &assets)
+{
+    SpriteTextRenderer<1> a;
+    a.addChar(C2D_SpriteSheetGetImage(assets.icons, 0));
+    return a;
+}
+
 void App::renderBottom()
 {
-    constexpr uint16_t WEEKDAY_WIDTH = Screen::BOTTOM_SCREEN_WIDTH / 4;
+    constexpr uint8_t BARS = 4;
+    constexpr uint16_t WEEKDAY_WIDTH = Screen::BOTTOM_SCREEN_WIDTH / BARS;
     constexpr uint8_t TOP_BAR_HEIGHT = 20;
     constexpr uint8_t BOTTOM_BAR_HEIGHT = 30;
+    static C2D_ImageTint tint;
+    C2D_PlainImageTint(&tint, C2D_Color32(255, 255, 255, 255), 0);
+
+    static SpriteTextRenderer<6> stuff = getBar(assets);
+    static SpriteTextRenderer<1> stuff2 = getIcon(assets);
 
     C2D_DrawRectSolid(0, 0, 0, Screen::BOTTOM_SCREEN_WIDTH, TOP_BAR_HEIGHT, C2D_Color32(7, 51, 149, 255));
-    C2D_DrawRectSolid(0, Screen::SCREEN_HEIGHT - BOTTOM_BAR_HEIGHT, 0, Screen::BOTTOM_SCREEN_WIDTH, BOTTOM_BAR_HEIGHT, C2D_Color32(7, 51, 149, 255));
-
-    for (uint8_t i = 0; i < 4; i++)
+    
+    for (uint8_t i = 0; i < BARS; i++)
     {
         uint32_t color = C2D_Color32(10 * (i + 1), 10 * (i + 1), 10 * (i + 1), 255);
-        C2D_DrawRectSolid(WEEKDAY_WIDTH * i, TOP_BAR_HEIGHT, 0, WEEKDAY_WIDTH, 100, color);
+        C2D_DrawRectSolid(WEEKDAY_WIDTH * i, TOP_BAR_HEIGHT, 0, WEEKDAY_WIDTH, 300, color);
+        stuff.render(WEEKDAY_WIDTH * (i+0.5) , 120, 0.1, &tint);
+        stuff2.render(WEEKDAY_WIDTH * (i+0.5) , 60, 0.5, &tint);
     }
-
+    
+    C2D_DrawRectSolid(0, Screen::SCREEN_HEIGHT - BOTTOM_BAR_HEIGHT, 0, Screen::BOTTOM_SCREEN_WIDTH, BOTTOM_BAR_HEIGHT, C2D_Color32(7, 51, 149, 255));
     C2D_DrawText(&assets.staticText.weeklyWeather, C2D_AlignCenter | C2D_AtBaseline | C2D_WithColor, Screen::BOTTOM_SCREEN_WIDTH / 2, TOP_BAR_HEIGHT * 0.7 + 2, 0, 0.6, 0.6, whiteColor);
     C2D_DrawText(&assets.staticText.home, C2D_AlignCenter | C2D_AtBaseline | C2D_WithColor, Screen::BOTTOM_SCREEN_WIDTH / 2, Screen::SCREEN_HEIGHT - BOTTOM_BAR_HEIGHT * 0.3, 0, 0.6, 0.6, whiteColor);
 }
--- source/include/graphics.h
+++ source/include/graphics.h
@@ -30,3 +30,71 @@ public:
         C2D_TextBufDelete(buffer);
     }
 };
+
+template <uint8_t Capacity>
+class SpriteTextRenderer
+{
+    struct
+    {
+        float scale = 1.0;
+        C2D_Image image;
+    } images[Capacity];
+    uint8_t textLength = 0;
+
+public:
+    void render(float xPos, float yPos, float scale, C2D_ImageTint *tint)
+    {
+        constexpr float gap = 30;
+        constexpr float space = 60;
+        float width = 0;
+        float height = 0;
+
+        for (uint8_t i = 0; i < textLength; i++)
+        {
+            bool last = i + 1 == textLength;
+            auto frame = images[i];
+            if (frame.scale == 0)
+            {
+                width += (space)*scale;
+            }
+            else
+            {
+                width += (frame.image.subtex->width + (last ? 0 : gap)) * scale * frame.scale;
+                height = std::max(height, frame.image.subtex->height * scale * frame.scale);
+            }
+        }
+
+        xPos -= width / 2;
+        yPos -= height / 2;
+
+        for (uint8_t i = 0; i < textLength; i++)
+        {
+            bool last = i + 1 == textLength;
+            auto frame = images[i];
+            if (frame.scale == 0)
+            {
+                xPos += (space)*scale;
+            }
+            else
+            {
+                C2D_DrawImageAt(frame.image, xPos, yPos + (height - frame.image.subtex->height * scale * frame.scale) / 2, 0, tint, scale * frame.scale, scale * frame.scale);
+                xPos += (frame.image.subtex->width + (last ? 0 : gap)) * scale * frame.scale;
+            }
+        }
+    }
+
+    void clear()
+    {
+        textLength = 0;
+    }
+
+    void addChar(C2D_Image image, float scale = 1.0)
+    {
+        images[textLength++] = {scale, image};
+    }
+
+    void addSpace()
+    {
+        images[textLength++].scale = 0;
+    }
+};
